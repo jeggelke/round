2db1ad6be742d92c299619123e68df80a2bec062
Jake Heggelke <jacob.heggelke@gmail.com>
2018-06-11 08:27:12 -0400

    fixed more spacing issues

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 	<footer id="colophon" class="site-footer container-fluid" role="contentinfo">
 		<div class="row social-footer">
 			<div class="col-xs-12">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
 					<div class="col-xs-12 col-sm-6 shop-info text-center footer-section">
 						<div class="footer-header">Visit Us!</div>
 						<div class="shop-address">
